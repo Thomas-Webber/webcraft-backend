713756b942a635f8e3775c568d56fec478b073f9
Thomas Webber <webber.thomas78@gmail.com>
2019-03-17 20:03:37 +0100

    updated dockerfile, add / endpoint

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,9 @@ COPY . .
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags="-w -s" -o webcraft
 
 FROM scratch
+
+ENV ENV=prod
+ENV PORT=80
+
 COPY --from=builder /app/webcraft webcraft
 ENTRYPOINT ["/go/bin/webcraft"]
--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"flag"
 	"github.com/Thomas-Webber/webcraft-backend/chat"
 	"github.com/Thomas-Webber/webcraft-backend/world"
 	"log"
 	"net/http"
+	"os"
 )
 
-var port = flag.String("port", "8888", "service port")
-var env = flag.String("env", "dev", "service port")
+func getEnv(name string, defaultValue string) string {
+	val := os.Getenv(name)
+	if len(val) == 0 {
+		return defaultValue
+	}
+	return val
+}
+
+var port = getEnv("PORT", "8888")
+var env = getEnv("ENV", "dev")
 
 func main() {
-	flag.Parse()
 	log.SetFlags(0)
-	if *env == "dev" {
+	if env == "dev" {
 		world.Upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 		chat.Upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	}
@@ -28,6 +35,11 @@ func main() {
 	world.World[world.EncodeXYZtoInt(15, 5, 5)] = 0xffffff
 	world.World[world.EncodeXYZtoInt(5, 5, 5)] = 0xff0000
 	http.HandleFunc("/get", world.GetHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("Hello")); err != nil {
+			log.Fatal(err)
+		}
+	})
 	http.HandleFunc("/action", world.ActionHandler)
 	http.HandleFunc("/reset", world.ResetHandler)
 
